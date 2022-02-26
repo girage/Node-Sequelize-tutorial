105e46cb234caa2aabc50fb7ef182eece66a280d
girage <45748977+girage@users.noreply.github.com>
2022-02-26 18:57:54 +0700

    done

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Node-Sequelize-tutorial
- 
+
+# Install Sequelize and DB Driver
+ npm install --save sequelize
+ npm install --save pg pg-hstore # Postgres
+
+# Create migration
+ npm install --save-dev sequelize-cli
+ npx sequelize-cli init
+
---
+++ migrations/20220226095258-init.js
@@ -0,0 +1,92 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('user', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+    });
+
+    await queryInterface.createTable('channel', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        reference: {
+          model: 'user',
+          key: 'id',
+        }
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+
+    await queryInterface.createTable('video', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      channel_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        reference: {
+          model: 'channel',
+          key: 'id',
+        }
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropAllTables();
+  },
+};
